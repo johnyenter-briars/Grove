1596dc57cd8ab6a6f41b2562583bd4a3b46237f4
JohnYenter-Briars <jyenterbriars@gmail.com>
2020-04-13 12:07:14 -0500

    refactor database connection to service model

--- src/app.py
+++ src/app.py
@@ -1,33 +1,15 @@
 from flask import Flask
 from flask import request, redirect
 from flask import render_template
-import sqlite3
-from flask import g
+from services.DatabaseService import DatabaseService
 
 app = Flask(__name__)
-_db = None
-# database = DatabaseService(
-DATABASE = 'database_files/Grove.db'
-
-def get_db():
-    _db = getattr(g, '_database', None)
-    if _db is None:
-        _db = g._database = sqlite3.connect(DATABASE)
-    return _db
-
-
-@app.teardown_appcontext
-def close_connection(exception):
-    _db = getattr(g, '_database', None)
-    if _db is not None:
-        _db.close()
-
+database = DatabaseService()
 
 @app.route('/')
 def hello():
-    db = get_db()
-    print(db.execute("select * from students;").fetchall())
-    return render_template("index.html", appName="The Grove")
+    # print(database.getStudents())
+    return render_template("index.html", appName="The Grove", students=database.getStudents())
 
 if __name__ == '__main__':
     app.run()
--- src/services/DatabaseService.py
+++ src/services/DatabaseService.py
@@ -1,27 +1,22 @@
 import sqlite3
-from flask import g
 
-DATABASE = 'database_files/Grove.db'
+DATABASE_PATH = 'database_files/Grove.db'
 
 class DatabaseService(object):
 
     def __init__(self):
         super().__init__()
-        self.get_db()
         self._db = None
-
+        self.set_db()
 
     def set_db(self):
-        self._db = getattr(g, '_database', None)
-        if self._db is None:
-            self._db = g._database = sqlite3.connect(DATABASE)
-        return self._db
-
+        self._db = sqlite3.connect(DATABASE_PATH, check_same_thread=False)
+        
     def get_db(self):
         return self._db
 
-    @app.teardown_appcontext
+    def getStudents(self):
+        return self._db.execute("select * from Students;").fetchall()
+
     def close_connection(self, exception):
-        self._db = getattr(g, '_database', None)
-        if self._db is not None:
-            self._db.close()
+        self._db.close()
