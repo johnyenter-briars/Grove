4f08e00c7249d46949232ed3e4e3724d07b00088
Murtaza Meerza <carbon.murtaza@hotmail.com>
2020-04-17 07:58:22 -0500

    Changes

--- src/app.py
+++ src/app.py
@@ -6,18 +6,22 @@ from services.DatabaseService import DatabaseService
 app = Flask(__name__)
 database = DatabaseService()
 
+
 @app.route('/')
 def hello():
     # print(database.getStudents())
     return render_template("index.html", appName="The Grove", students=database.getStudents())
 
+
 @app.route('/home')
-def projects():
+def home():
     return render_template("home.html")
+
+
+@app.route('/projects')
+def projects():
+    return render_template("projects.html")
+
 
 if __name__ == '__main__':
     app.run()
-   
-
-
-
