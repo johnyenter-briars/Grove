27fb4c2544b2f0ed074afe46112337cd199e95c6
JohnYenter-Briars <jyenterbriars@gmail.com>
2020-04-20 19:01:09 -0500

    Add One Unit test for database service

--- src/test/UnitTestSuite.py
+++ src/test/UnitTestSuite.py
@@ -1,13 +1,18 @@
 import unittest 
 import sys
 import os
-from ..services.DatabaseService import DatabaseService
+sys.path.append(".")
+from services.DatabaseService import DatabaseService
 
+"""This unit test MUST be run from the src directory using:
+    python test/UnitTestSuite.py
+"""
 class SimpleTest(unittest.TestCase): 
   
     # Returns True or False.  
     def test(self):   
         d = DatabaseService()
+        assert('Bruce' == d.getStudent(1).getFirstName())
 
 if __name__ == '__main__': 
     unittest.main() 
