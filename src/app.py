6cc57d5a755e99649d82c7dd07c066c6379b04fa
Patrick Fisher <patrick.fisher9999@gmail.com>
2020-04-22 19:40:48 -0500

    more work done on profile page

--- src/app.py
+++ src/app.py
@@ -79,9 +79,11 @@ def task():
 @app.route('/profile')
 def profile():
     sess = json.loads(session['user_auth'])
+    branches = database.getBranchesForStudent(sess.get('_StudentID'))
+    awards = database.getAwardsForStudent(sess.get('_StudentID'))
     first = sess.get('_FirstName')
     last = sess.get('_LastName')
-    return render_template("profile.html", name=first+' '+last)
+    return render_template("profile.html", name=first+' '+last, branches=branches, awards=awards)
 
 @app.route('/logout')
 def logout():
