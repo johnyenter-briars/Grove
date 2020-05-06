8bedbffdd4906446e10ee3d10caee8d394b69504
Patrick Fisher <patrick.fisher9999@gmail.com>
2020-05-06 18:21:16 -0500

    wip delete

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,8 +1,14 @@
-function deleteFile(fname) {
+async function deleteFile(fname) {
     var xhr = new XMLHttpRequest();
     var formData = new FormData();
-    formData.append("filename", fname)
-    xhr.open("POST", "/task/", true);
+    formData.append("filename", fname);
+    console.log(fname);
+    xhr.open("POST", "/task/{{taskID}}", true);
     //xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(formData);
+    await xhr.send(formData);
+    await this.reload();
+    return false;
+}
+function reload(){
+    window.location.reload(true);
 }
