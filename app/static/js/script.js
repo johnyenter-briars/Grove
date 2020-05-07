66e3bc551c7d906245d37de48679e0ec71b5ecca
Patrick Fisher <patrick.fisher9999@gmail.com>
2020-05-06 19:20:49 -0500

    finalize file deletion

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,15 +1,1 @@
 console.log("hello there!");
-async function deleteFile(fname) {
-    var xhr = new XMLHttpRequest();
-    var formData = new FormData();
-    formData.append("filename", fname);
-    console.log(fname);
-    xhr.open("POST", "/task/{{taskID}}", true);
-    //xhr.setRequestHeader('Content-Type', 'application/json');
-    await xhr.send(formData);
-    await this.reload();
-    return false;
-}
-function reload(){
-    window.location.reload(true);
-}
