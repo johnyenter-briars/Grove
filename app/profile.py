1a9102543b57351971bee295c269d219fcc25074
Patrick Fisher <patrick.fisher9999@gmail.com>
2020-05-06 20:17:17 -0500

    Add tasks to profile page

--- app/profile.py
+++ app/profile.py
@@ -6,6 +6,7 @@ def profile():
     sess = json.loads(session['user_auth'])
     branches = database.getBranchesForStudent(sess.get('_StudentID'))
     awards = database.getAwardsForStudent(sess.get('_StudentID'))
+    tasks = database.getTasksForStudent(sess.get('_StudentID'))
     first = sess.get('_FirstName')
     last = sess.get('_LastName')
-    return render_template("profile.html", name=first+' '+last, branches=branches, awards=awards)
+    return render_template("profile.html", name=first+' '+last, branches=branches, awards=awards, tasks=tasks)
