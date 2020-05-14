0b45cb661e2c6763e07908dbb47507c63b672169
eselenic <selenicaelona@gmail.com>
2020-05-13 19:20:25 -0500

    lil mishap

--- app/profile.py
+++ app/profile.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, redirect, render_template, json, session, jsonify,url_for
 from app import app, database
+from exceptions.NoProfileIDException import NoProfileIDException
 
 @app.route('/profile/')
 def profile():
+    if request.args.get('profileID') == None:
+        raise NoProfileIDException
     currentProfileID = request.args.get('profileID')
     sess = json.loads(session['user_auth'])
     branches = database.getBranchesForStudent(currentProfileID)
