279c817863c3f70a0b48def737677461edeb6f18
lucasmwelsh <59670629+lucasmwelsh@users.noreply.github.com>
2020-05-19 16:00:22 -0500

    Made the taskbar different for students vs teachers

--- app/profile.py
+++ app/profile.py
@@ -8,9 +8,11 @@ def profile():
         raise NoProfileIDException
     currentProfileID = request.args.get('profileID')
     sess = json.loads(session['user_auth'])
+    studentID = sess.get('_StudentID')
+    teacherID = sess.get('_TeacherID')
     branches = database.getBranchesForStudent(currentProfileID)
     awards = database.getAwardsForStudent(currentProfileID)
     tasks = database.getTasksForStudent(currentProfileID)
     first = database.getStudent(currentProfileID).getFirstName() 
     last = database.getStudent(currentProfileID).getLastName() 
-    return render_template("profile.html", name=first+' '+last, branches=branches, awards=awards, tasks=tasks)
+    return render_template("profile.html", name=first+' '+last, branches=branches, awards=awards, tasks=tasks, studentID=studentID, teacherID=teacherID)
