5b5a174e0cfdf9150c66332916be832246a5ad0f
eselenic <selenicaelona@gmail.com>
2020-05-27 23:30:30 -0500

    adding js code

--- app/static/js/awardapples.js
+++ app/static/js/awardapples.js
@@ -5,3 +5,23 @@ function clearSelection(){
         radioElement.checked = false;
     }
 }
+
+function checkElements(){
+    var $rb = $('input:radio')
+    var djangoData = $('#my-data').data().name;
+    var count = 0;
+    var showAlert = true;
+    $('#my_radio_box').submit(function(){
+        for (var i = 0; i < $rb.length; i += 1) {
+            if ($($rb[i]).is(':checked')) {            
+                count++;
+            }
+        }
+        if(count > djangoData && showAlert) {
+            showAlert = false;
+            alert("You are exceeding your limit. You may only submit " + djangoData);
+            return false;
+        }
+
+    });
+}
